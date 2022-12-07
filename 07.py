ddf3f3cb5814ba8b8c24499d4173168352cb2b7b
Daniel Messias <danielcmessias@gmail.com>
2022-12-07 15:19:06 +0000

    Day 7

---
+++ 07.py
@@ -0,0 +1,81 @@
+"""
+This solution is a bit gross, should have just used Pathlib. Oh well...
+"""
+
+from collections import defaultdict
+from functools import reduce
+from operator import getitem
+from typing import List
+from util import load
+
+
+def get_in_dict(d: dict, keys: List):
+    return reduce(getitem, keys, d)
+
+
+def set_in_dict(d: dict, keys: List, value):
+    get_in_dict(d, keys[:-1])[keys[-1]] = value
+
+
+def cd(cmd, currentPath) -> List[str]:
+    if cmd == "/":
+        return ["/"]
+    elif cmd == "..":
+        return currentPath[:-1]
+    else:
+        return currentPath + [cmd]
+
+
+def _make_tree():
+    return defaultdict(_make_tree)
+
+
+def parseTree(lines: List[str]):
+    tree = _make_tree()
+    currentPath = ["/"]
+    for l in lines:
+        args = l.split()
+        if l.startswith("$"):
+            cmd = args[1]
+            if cmd == "cd":
+                currentPath = cd(args[2], currentPath)
+        else:
+            # ls
+            dirPath = currentPath + [args[1]]
+            if args[0] == "dir":
+                get_in_dict(tree, dirPath)
+            else:
+                set_in_dict(tree, dirPath, args[0])
+
+    return tree
+
+
+def sumDirectory(tree, path=[], all_dirs=[]):
+    total = 0
+    l = []
+    for k, v in tree.items():
+        if isinstance(v, dict):
+            vTotal, vDirs = sumDirectory(v, path + [k], all_dirs)
+            l.extend(vDirs)
+            total += vTotal
+        else:
+            total += int(v)
+
+    return total, l + [("/".join(path), total)]
+
+
+def solve(data: str):
+    tree = parseTree(data.splitlines())
+
+    used_space, all_dirs = sumDirectory(tree)
+    print("Part 1:", sum([s[1] for s in all_dirs if s[1] <= 100000]))
+
+    min_delete_size = 30000000 - (70000000 - used_space)
+    viable_dirs = sorted(
+        [d for d in all_dirs if d[1] >= min_delete_size], key=lambda x: x[1]
+    )
+    print("Part 2:", viable_dirs[0][1])
+
+
+if __name__ == "__main__":
+    solve(load(7))
